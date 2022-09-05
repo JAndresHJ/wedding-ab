897462379cb167338a4298e521ba780d14fa9656
Andres <jandreshdzj@gmail.com>
2022-09-05 10:05:46 -0500

    Fix counter

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,73 +1,90 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
 
-function Countdown () {
+function Countdown() {
+  const [dateReached, setDateReached] = useState(true);
+
   useEffect(() => {
-    (function () {
-      const second = 1000,
-            minute = second * 60,
-            hour = minute * 60,
-            day = hour * 24;
-      let birthday = "Dec 15, 2021 00:00:00",
-          countDown = new Date(birthday).getTime(),
-          x = setInterval(function() {    
-            let now = new Date().getTime(),
-                distance = countDown - now;
-    
-            document.getElementById("days").innerText = Math.floor(distance / (day));
-            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour));
-            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute));
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-    
-            //do something later when date is reached
-            if (distance < 0) {
-              let headline = document.getElementById("headline"),
-                  countdown = document.getElementById("countdown"),
-                  content = document.getElementById("content");
-    
-              headline.innerText = "It's our wedding!";
-              countdown.style.display = "none";
-              content.style.display = "block";
-    
-              clearInterval(x);
-            }
-            //seconds
-          }, 0);
-          return null;
-      }());
-  }, [])
+    const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+    const weddingDate = 'Mar 19, 2023 00:00:00';
+    const countDown = new Date(weddingDate).getTime();
+
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countDown - now;
+
+      if (!dateReached) {
+        document.getElementById('days').innerText = Math.floor(distance / day);
+        document.getElementById('hours').innerText = Math.floor(
+          (distance % day) / hour
+        );
+        document.getElementById('minutes').innerText = Math.floor(
+          (distance % hour) / minute
+        );
+        document.getElementById('seconds').innerText = Math.floor(
+          (distance % minute) / second
+        );
+      }
+
+      //do something later when date is reached
+      if (distance < 0) {
+        setDateReached(true);
+
+        clearInterval(interval);
+      }
+    }, 0);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [dateReached]);
+
   return (
     <div
       id='countdown'
       className='section-padding bg-img bg-fixed'
       data-background='images/banner-1.jpg'
     >
-      <div className='container'>
-        <div className='row'>
-          <div className='section-head col-md-12'>
-            <h4>We will become a family in</h4>
+      {!dateReached ? (
+        <div className='container'>
+          <div className='row'>
+            <div className='section-head col-md-12'>
+              <h4>We will become a family in</h4>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col-md-12'>
+              <ul>
+                <li>
+                  <span id='days'></span>Days
+                </li>
+                <li>
+                  <span id='hours'></span>Hours
+                </li>
+                <li>
+                  <span id='minutes'></span>Minutes
+                </li>
+                <li>
+                  <span id='seconds'></span>Seconds
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-        <div className='row'>
-          <div className='col-md-12'>
-            <ul>
-              <li>
-                <span id='days'></span>Days
-              </li>
-              <li>
-                <span id='hours'></span>Hours
-              </li>
-              <li>
-                <span id='minutes'></span>Minutes
-              </li>
-              <li>
-                <span id='seconds'></span>Seconds
-              </li>
-            </ul>
+      ) : (
+        <div className='container'>
+          <div className='row'>
+            <div className='section-head col-md-12'>
+              <h4>Hoy es nuestra boda!! Acompañanos</h4>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Countdown
+export default Countdown;
